1fda88656e820536153cb2a4327effded058fa3b
myuon <ioi.joi.koi.loi@gmail.com>
2025-01-31 01:39:54 +0900

    feat: Arm64 struct

--- src/jit.zig
+++ src/jit.zig
@@ -3,7 +3,37 @@ const fs = std.fs;
 const mman = @cImport(@cInclude("sys/mman.h"));
 const pthread = @cImport(@cInclude("pthread.h"));
 
-pub fn compileJit() !*fn () callconv(.C) void {
+const Arm64 = struct {
+    code_buf: std.ArrayList(u32),
+
+    pub fn init(allocator: std.mem.Allocator) Arm64 {
+        return Arm64{
+            .code_buf = std.ArrayList(u32).init(allocator),
+        };
+    }
+
+    pub fn deinit(self: *Arm64) void {
+        self.code_buf.deinit();
+    }
+
+    fn emit(self: *Arm64, instr: u32) anyerror!void {
+        try self.code_buf.append(instr);
+    }
+
+    fn emitRet(self: *Arm64) anyerror!void {
+        try self.emit(0xd65f03c0);
+    }
+
+    fn emitPopX0(self: *Arm64) anyerror!void {
+        try self.emit(0xf84107e1); // ldr x1, [sp], 16
+    }
+
+    fn emitPushX0(self: *Arm64) anyerror!void {
+        try self.emit(0xf81f0fe0); // str x0, [sp, -16]!
+    }
+};
+
+pub fn compileJit() anyerror!*fn () callconv(.C) void {
     const buf = mman.mmap(
         null,
         4096,
@@ -15,13 +45,16 @@ pub fn compileJit() !*fn () callconv(.C) void {
 
     const buf_ptr: [*]u32 = @ptrCast(@alignCast(buf));
 
+    var code = Arm64.init(std.testing.allocator);
+    defer code.deinit();
+
+    try code.emit(0xd2824680); // mov x0, #0x1234
+    try code.emitPushX0();
+    try code.emitPopX0();
+    try code.emitRet();
+
     pthread.pthread_jit_write_protect_np(0);
-    @memcpy(
-        buf_ptr,
-        &[_]u32{
-            0xd65f03c0, // ret
-        },
-    );
+    @memcpy(buf_ptr, code.code_buf.items);
     pthread.pthread_jit_write_protect_np(1);
 
     return @ptrCast(@alignCast(buf));
