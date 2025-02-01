1c31cf016bc951381b30374ade62820784e1f556
myuon <ioi.joi.koi.loi@gmail.com>
2025-02-01 20:29:47 +0900

    feat: push c_stacks

--- src/jit.zig
+++ src/jit.zig
@@ -5,6 +5,7 @@ const pthread = @cImport(@cInclude("pthread.h"));
 
 const ast = @import("ast.zig");
 
+const Register = enum(u32) { x0 = 0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24, x25, x26, x27, x28, x29, x30 };
 const Arm64 = struct {
     code_buf: std.ArrayList(u32),
 
@@ -26,16 +27,41 @@ const Arm64 = struct {
         try self.emit(0xd65f03c0); // ret x30
     }
 
-    fn emitMovImm(self: *Arm64, target: u8, imm: u16) anyerror!void {
-        try self.emit(0xD2800000 | (@as(u32, imm) << 5) | @as(u32, target));
+    fn emitMovImm(self: *Arm64, target: Register, imm: u16) anyerror!void {
+        try self.emit(0xD2800000 | (@as(u32, imm) << 5) | @as(u32, @intFromEnum(target)));
     }
 
-    fn emitStr(
-        self: *Arm64,
-        base: u8,
-        value: u8,
-    ) anyerror!void {
-        try self.emit(0xF9000000 | (0x0 << 10) | (@as(u32, base) << 5) | @as(u32, value));
+    fn emitMov(self: *Arm64, source: Register, target: Register) anyerror!void {
+        try self.emit(0xAA000000 | (@as(u32, @intFromEnum(source)) << 16 | @as(u32, @intFromEnum(source)) << 5 | @as(u32, @intFromEnum(target))));
+    }
+
+    fn emitStr(self: *Arm64, base: Register, value: Register) anyerror!void {
+        try self.emit(0xF9000000 | (0x0 << 10) | (@as(u32, @intFromEnum(base) << 5) | @as(u32, @intFromEnum(value))));
+    }
+
+    fn emitLdr(self: *Arm64, offset: u9, source: Register, target: Register) anyerror!void {
+        try self.emit(0xF8400400 | (@as(u32, offset) << 12) | (@as(u32, @intFromEnum(source) << 5) | @as(u32, @intFromEnum(target))));
+    }
+
+    fn emitAdd(self: *Arm64, source1: Register, source2: Register, target: Register) anyerror!void {
+        try self.emit(0x8B000000 | (@as(u32, @intFromEnum(source1) << 16) | @as(u32, @intFromEnum(source2) << 5) | @as(u32, @intFromEnum(target))));
+    }
+
+    fn emitAddImm(self: *Arm64, source: Register, imm: u12, target: Register) anyerror!void {
+        try self.emit(0x91000000 | (@as(u32, imm) << 10) | @as(u32, @intFromEnum(source) << 5) | @as(u32, @intFromEnum(target)));
+    }
+
+    fn emitMul(self: *Arm64, source1: Register, source2: Register, target: Register) anyerror!void {
+        try self.emit(0x9B00FC00 | (@as(u32, @intFromEnum(source1)) << 16) | (@as(u32, @intFromEnum(source2)) << 5) | @as(u32, @intFromEnum(target)));
+    }
+
+    fn emitMadd(self: *Arm64, target: Register, source1: Register, source2: Register, base: Register) anyerror!void {
+        const t: u32 = @intFromEnum(target);
+        const b: u32 = @intFromEnum(base);
+        const s1: u32 = @intFromEnum(source1);
+        const s2: u32 = @intFromEnum(source2);
+
+        try self.emit(0x9B000000 | (s1 << 16) | (b << 10) | (s2 << 5) | t);
     }
 };
 
@@ -59,29 +85,29 @@ pub fn compileJit(prog: []ast.Instruction) anyerror!CompiledFn {
     var code = Arm64.init(std.testing.allocator);
     defer code.deinit();
 
-    // for (prog) |inst| {
-    //     switch (inst) {
-    //         .push => |_| {
-    //             // try code.emitMovkX9Imm(@intCast(n));
-    //             // try code.emitPushX9();
-    //             try code.emit(0xD2800009);
-    //             try code.emit(0xF81F8FE9);
-    //         },
-    //         .ret => {
-    //             // try code.emitPopX9();
-    //             try code.emitRet();
-    //         },
-    //         else => {},
-    //     }
-    // }
+    const reg_c_stack = Register.x0;
+    const reg_c_sp = Register.x1;
 
-    _ = prog;
+    for (prog) |inst| {
+        switch (inst) {
+            .push => |n| {
+                try code.emitLdr(0, reg_c_sp, .x9);
+                try code.emitMovImm(.x11, 0x8);
+                try code.emitMadd(.x10, .x11, .x9, reg_c_stack);
 
-    try code.emitMovImm(0x9, 1234);
-    try code.emitStr(0x0, 0x9);
+                try code.emitMovImm(.x9, @intCast(n));
+                try code.emitStr(.x10, .x9);
 
-    try code.emitMovImm(0x9, 1);
-    try code.emitStr(0x1, 0x9);
+                try code.emitLdr(0, reg_c_sp, .x9);
+                try code.emitAddImm(.x9, 0x1, .x9);
+                try code.emitStr(reg_c_sp, .x9);
+            },
+            .ret => {
+                try code.emitRet();
+            },
+            else => {},
+        }
+    }
 
     try code.emitRet();
 
@@ -95,15 +121,34 @@ pub fn compileJit(prog: []ast.Instruction) anyerror!CompiledFn {
 }
 
 test {
-    const fn_ptr = try compileJit(@constCast(&[_]ast.Instruction{
-        .{ .push = 0x12 },
-        .{ .ret = true },
-    }));
+    const cases = [_]struct {
+        prog: []ast.Instruction,
+        expected: struct {
+            c_stack: []i64,
+            c_sp: i64,
+        },
+    }{
+        .{
+            .prog = @constCast(&[_]ast.Instruction{
+                .{ .push = 0x12 },
+                .{ .push = 0x34 },
+                .{ .push = 0x56 },
+                .{ .ret = true },
+            }),
+            .expected = .{
+                .c_stack = @constCast(&[_]i64{ 0x12, 0x34, 0x56, 0, 0 }),
+                .c_sp = 3,
+            },
+        },
+    };
 
-    var k: i64 = 0;
+    for (cases) |c| {
+        var c_sp: i64 = 0;
+        var c_stack = [_]i64{ 0, 0, 0, 0, 0 };
+        const fn_ptr = try compileJit(c.prog);
+        fn_ptr(@constCast((&c_stack).ptr), @constCast(&c_sp));
 
-    var stack = [_]i64{ 0, 0, 0, 0, 0 };
-    fn_ptr(@constCast((&stack).ptr), @constCast(&k));
-
-    std.debug.print("c_stack: {any}, c_sp: {d}\n", .{ stack, k });
+        try std.testing.expectEqualSlices(i64, c.expected.c_stack, &c_stack);
+        try std.testing.expectEqual(c.expected.c_sp, c_sp);
+    }
 }
