e751845a51bc86718140299c294366b5159f0468
myuon <ioi.joi.koi.loi@gmail.com>
2025-02-11 12:14:43 +0900

    feat: read a file and run it!

--- src/main.zig
+++ src/main.zig
@@ -20,7 +20,7 @@ pub fn main() !void {
 
     const argv = std.os.argv;
     if (argv.len < 2) {
-        return std.debug.print("Usage: orbit [fib, prime, dbg]\n", .{});
+        return std.debug.print("Usage: orbit [fib, prime, run, dbg]\n", .{});
     }
 
     const stdout_file = std.io.getStdOut().writer();
@@ -28,63 +28,22 @@ pub fn main() !void {
     const stdout = bw.writer();
 
     const command = argv[1][0..std.mem.len(argv[1])];
-    if (std.mem.eql(u8, command, "fib")) {
-        const result = try c.evalModule(
-            \\fun fib(n) do
-            \\  if (n == 0) do
-            \\    return 1;
-            \\  end
-            \\  if (n == 1) do
-            \\    return 1;
-            \\  end
-            \\
-            \\  return fib(n - 1) + fib(n - 2);
-            \\end
-            \\
-            \\fun main() do
-            \\  return fib(35);
-            \\end
-        );
+    if (std.mem.eql(u8, command, "run")) {
+        var file = try std.fs.cwd().openFile(argv[2][0..std.mem.len(argv[2])], .{});
+        defer file.close();
+
+        var content = std.ArrayList(u8).init(allocator);
+        defer content.deinit();
+        while (try file.reader().readUntilDelimiterOrEofAlloc(allocator, '\n', std.math.maxInt(usize))) |line| {
+            defer allocator.free(line);
+
+            try content.appendSlice(line);
+        }
+
+        const result = try c.evalModule(content.items);
 
         try stdout.print("Result: {any}\n", .{result});
         try bw.flush();
-    } else if (std.mem.eql(u8, command, "prime")) {
-        const result = try c.evalModule(
-            \\fun is_prime(n) do
-            \\  if (n < 2) do
-            \\    return false;
-            \\  end
-            \\
-            \\  let i = 2;
-            \\  while (i * i <= n) do
-            \\    if (n % i == 0) do
-            \\      return false;
-            \\    end
-            \\    i = i + 1;
-            \\  end
-            \\
-            \\  return true;
-            \\end
-            \\
-            \\fun main() do
-            \\  let n = 1000;
-            \\  let sum = 0;
-            \\
-            \\  while (n > 0) do
-            \\    n = n - 1;
-            \\
-            \\    if (is_prime(n)) do
-            \\      sum = sum + n;
-            \\    end
-            \\  end
-            \\
-            \\  return sum;
-            \\end
-        );
-
-        try stdout.print("Result: {any}\n", .{result});
-
-        try bw.flush(); // don't forget to flush!
     } else if (std.mem.eql(u8, command, "dbg")) {
         try debugger.start(allocator);
     } else {
